4bd81f3b4b42a7f3633dfec582973dbd8246d244
daniacca <d.acca87@gmail.com>
2024-02-19 10:38:43 +0100

    feat: added unweithed graph data structure

---
+++ DataStructure/Graph/Graph.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Graph
+{
+    public class Graph<T>
+    {
+        private readonly Dictionary<T, HashSet<T>> adjacencyList = new();
+        public int Vertexs { get; private set; } = 0;
+        public int Edges { get; private set; } = 0;
+
+        public void AddVertex(T s)
+        {
+            adjacencyList.Add(s, new HashSet<T>());
+            Vertexs++;
+        }
+
+        public void AddEdge(T source, T destination, bool bidirectional = true)
+        {
+            if (!adjacencyList.ContainsKey(source))
+                AddVertex(source);
+
+            if (!adjacencyList.ContainsKey(destination))
+                AddVertex(destination);
+
+            adjacencyList[source].Add(destination);
+            if (bidirectional)
+                adjacencyList[destination].Add(source);
+
+            Edges++;
+        }
+
+        public bool RemoveEdge(T source, T destination)
+        {
+            var removed = false;
+
+            if (adjacencyList.TryGetValue(source, out var sourceList))
+            {
+                removed |= sourceList.Remove(destination);
+            }
+
+            if (adjacencyList.TryGetValue(destination, out var destList))
+            {
+                removed = destList.Remove(source);
+            }
+
+            if (removed)
+                Edges--;
+
+            return removed;
+        }
+
+        public bool RemoveVertex(T vertex)
+        {
+            var removed = false;
+
+            if(adjacencyList.ContainsKey(vertex))
+                removed |= adjacencyList.Remove(vertex);
+
+            foreach (var v in adjacencyList)
+            {
+                v.Value.Remove(vertex);
+            }
+
+            if(removed)
+                Vertexs--;
+
+            return removed;
+        }
+
+        public void Clear()
+        {
+            adjacencyList.Clear();
+        }
+
+        private bool DFS(T start, T goal, HashSet<T> visited)
+        {
+            visited.Add(start);
+
+            var destinations = adjacencyList[start];
+            foreach (var destination in destinations)
+            {
+                if (destination.Equals(goal))
+                {
+                    return true;
+                }
+
+                if (!visited.Contains(destination))
+                {
+                    return DFS(destination, goal, visited);
+                }
+            }
+
+            return false;
+        }
+
+        public bool DFS(T start, T goal)
+        {
+            return DFS(start, goal, new HashSet<T>());
+        }
+
+        public bool BFS(T start, T goal)
+        {
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            var visited = new HashSet<T>
+            {
+                start
+            };
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                var destinations = adjacencyList[node];
+
+                foreach (var destination in destinations)
+                {
+                    if (destination.Equals(goal))
+                        return true;
+
+                    if (!visited.Contains(destination))
+                    {
+                        queue.Enqueue(destination);
+                        visited.Add(destination);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool DistanceBFS(T src, T goal, Dictionary<T, T> pred, Dictionary<T, int> dist)
+        {
+            var queue = new Queue<T>();
+            var visited = new HashSet<T>
+            {
+                src
+            };
+
+            dist[src] = 0;
+            queue.Enqueue(src);
+
+            while (queue.Count > 0)
+            {
+                var u = queue.Dequeue();
+                var destinations = adjacencyList[u];
+
+                foreach (var destination in destinations)
+                {
+                    if (!visited.Contains(destination))
+                    {
+                        visited.Add(destination);
+                        pred[destination] = u;
+
+                        var prevDistance = 0;
+                        dist.TryGetValue(u, out prevDistance);
+                        dist[destination] = prevDistance + 1;
+
+                        queue.Enqueue(destination);
+
+                        if (destination.Equals(goal))
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static (T s, T d)[] GetPath(List<T> visited)
+        {
+            var output = new List<(T s, T d)>();
+
+            for (int i = 0; i < visited.Count - 1; i++)
+            {
+                var s = visited[i];
+                var d = visited[i + 1];
+                output.Add((s, d));
+            }
+
+            return output.ToArray();
+        }
+
+        public (T s, T d)[] ShortestPath(T start, T goal)
+        {
+            var pred = new Dictionary<T, T>();
+            var dist = new Dictionary<T, int>();
+
+            if (!DistanceBFS(start, goal, pred, dist))
+            {
+                return Enumerable.Empty<(T s, T d)>().ToArray();
+            }
+
+            var path = new List<T>();
+            T crawl = goal;
+            path.Add(crawl);
+
+            while (pred.ContainsKey(crawl))
+            {
+                path.Add(pred[crawl]);
+                crawl = pred[crawl];
+            }
+
+            return GetPath(path);
+        }
+    }
+}
---
+++ DataStructures.Tests/GraphTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructure.Graph;
+using Xunit;
+
+namespace DataStructure.Tests
+{
+    public class GraphTests
+    {
+        [Fact]
+        public void Should_Create_Graph_And_Add_Vertex()
+        {
+            var g = new Graph<int>();
+
+            Assert.NotNull(g);
+            Assert.Equal(0, g.Vertexs);
+            Assert.Equal(0, g.Edges);
+
+            g.AddVertex(1);
+            g.AddVertex(2);
+
+            Assert.Equal(2, g.Vertexs);
+        }
+
+        [Fact]
+        public void Should_Create_Graph_And_Add_Edges()
+        {
+            var g = new Graph<int>();
+
+            Assert.NotNull(g);
+            Assert.Equal(0, g.Vertexs);
+            Assert.Equal(0, g.Edges);
+
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 3);
+            g.AddEdge(2, 4);
+            g.AddEdge(3, 4);
+
+            Assert.Equal(4, g.Vertexs);
+            Assert.Equal(4, g.Edges);
+        }
+    }
+}
